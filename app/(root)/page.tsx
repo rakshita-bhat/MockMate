2e11205819fd7b834dfd2659b7f38085888011b9
rakshita-bhat <rakshitabhat2004@gmail.com>
2025-11-13 21:49:38 +0530

    sig-in and sign-up pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+
 
 const page = () => {
   return (
-    <div>
-      Home Page
-    </div>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
+        
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+            </Button>
+          </div>
+      </section>
+    </>  
   )
 }
 
