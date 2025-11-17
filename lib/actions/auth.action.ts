5edb1278a8d54f9ad0ed17a13e89c44e7e55ae53
rakshita-bhat <rakshitabhat2004@gmail.com>
2025-11-17 19:57:40 +0530

    Frontend

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -3,9 +3,10 @@
 import { auth, db } from "@/firebase/admin";
 import { cookies } from "next/headers";
 
-
+// Session duration (1 week)
 const SESSION_DURATION = 60 * 60 * 24 * 7;
 
+// Set session cookie
 export async function setSessionCookie(idToken: string) {
   const cookieStore = await cookies();
 
@@ -86,4 +87,46 @@ export async function signIn(params: SignInParams) {
       message: "Failed to log into account. Please try again.",
     };
   }
+}
+
+// Sign out user by clearing the session cookie
+export async function signOut() {
+  const cookieStore = await cookies();
+
+  cookieStore.delete("session");
+}
+
+// Get current user from session cookie
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    // get user info from db
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
+
+    // Invalid or expired session
+    return null;
+  }
+}
+
+// Check if user is authenticated
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+  return !!user;
 }
